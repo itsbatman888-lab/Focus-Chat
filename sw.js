55d694f1b95dd9d2767bc7e573f8241c27e5fb25
itsbatman888-lab <its.batman.888@gmail.com>
2026-06-23 20:54:05 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'focuschat-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  '1000038004.jpg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
